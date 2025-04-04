3f2f31682cc2a8f2ab2ab8726e19c968dd2052f3
Nilesh Chaudhari <140596453+nileshc13@users.noreply.github.com>
2025-04-04 09:58:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
+terraform {     
+        backend "remote" {
+         # The name of your Terraform Cloud organization.
+           organization = "Demo-Project"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+           name = "demo-workspace"
+         }
+      }
+    
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example"
-  location = "central india"
+  location = "central us"
 }
